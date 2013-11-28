3f0ef57eee50c8c4af63a1afdc6f7af0607d1715
akindemirci <akin.demirci@mono.net.tr>
2013-11-28 13:21:00 -0500

    admin.layout.php'de unutulan bir duzeltme

--- views/layouts/admin.html.php
+++ views/layouts/admin.html.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>	
+<head>
 	<meta charset="utf-8">
 	<title><?=$hotspot['marka']?></title>
 	<link href="assets/css/style.css" rel="stylesheet" />
@@ -12,36 +12,36 @@
 </head>
 <body>
 	<div class="container">
-	
+
 	<h1><?=$hotspot['marka']?> Yönetim Paneli</h1>
-	
+
 	<?php
 	if (date('Y', strtotime($hotspot['demo_bitis'])) != '2010')
 		echo '<div style="color:#e00;font-weight:bold">Hotspot demo süresi ' . $hotspot['demo_bitis'] . ' tarihinde dolacaktır.</div>';
 	?>
-	
+
 	<div id="nav">
-		<a href="<?=url_for('users')?>">Kullanıcı Listesi</a> | 
-	<? if ($settings['authentication'] == 'sms'): ?>
-		<a href="<?=url_for('sms')?>">SMS Raporu</a> | 
+		<a href="<?=url_for('users')?>">Kullanıcı Listesi</a> |
+	<? if (isset($settings['authentication']['sms'])): ?>
+		<a href="<?=url_for('sms')?>">SMS Raporu</a> |
 	<? endif; ?>
-		<a href="<?=url_for('settings')?>">Ayarlar</a> | 
-		<a href="<?=url_for('lang/tr')?>">Dil</a> | 
+		<a href="<?=url_for('settings')?>">Ayarlar</a> |
+		<a href="<?=url_for('lang/tr')?>">Dil</a> |
 		<a href="/">pfSense</a>
 	</div>
-	
+
 	<? if (isset($_SESSION['message'])): ?>
 		<div class="message"><?=$_SESSION['message']?></div>
 	<? unset($_SESSION['message']); endif; ?>
-		
+
 	<div class="content">
 		<?= $content; ?>
 	</div>
-	
+
 	<footer>
 		<p>© <?=$hotspot['firma']?> 2013</p>
 	</footer>
-	
+
 	</div>
 </body>
 </html>
