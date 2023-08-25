4809aba05dac779fb5b7227b840f82fc39ca5fd5
mt190592 <mt190502@mtaha.dev>
2023-08-25 21:20:19 +0300

    update

--- monospot-setup.sh
+++ monospot-setup.sh
@@ -3,7 +3,7 @@
 #~ make new workfolder & get tools and configs
 [ ! -d /root/monospot ] && mkdir /root/monospot
 cd /root/monospot
-[ ! -e $PWD/monospot-setup.conf ] && { echo "\"monospot-setup.conf\" dosyasi bulunamadi, git uzerinden cekilecek..."; fetch https://raw.githubusercontent.com/monobilisim/monospot/master/monospot-setup.conf; echo "\"monospot-setup.conf\"" dosyasini editor araciligi ile duzenledikten sonra \"sh monospot-setup.sh\" komutu ile bu scripti tekrardan calistirin; exit 0; }
+[ ! -e $PWD/monospot-setup.conf ] && { echo "\"$PWD/monospot-setup.conf\" dosyasi bulunamadi, git uzerinden cekilecek..."; fetch https://raw.githubusercontent.com/monobilisim/monospot/master/monospot-setup.conf; echo "\"$PWD/monospot-setup.conf\"" dosyasini editor araciligi ile duzenledikten sonra \"sh monospot-setup.sh\" komutu ile bu scripti tekrardan calistirin; exit 0; }
 . $PWD/monospot-setup.conf
 
 #~ install monospot
@@ -11,7 +11,7 @@ cd /root/monospot
 [ -d /usr/local/captiveportal/monospot     ] && mv /usr/local/captiveportal/monospot /usr/local/captiveportal/monospot.bak
 [ -d /usr/local/www/monospot               ] && rm /usr/local/www/monospot
 fetch https://github.com/monobilisim/monospot/archive/master.zip -o $PWD/monospot.zip
-unzip -d /usr/local/captiveportal/ $PWD/monospot.zip
+unzip -d /usr/local/captiveportal $PWD/monospot.zip
 mv /usr/local/captiveportal/monospot-master /usr/local/captiveportal/monospot
 ln -sf /usr/local/captiveportal/monospot /usr/local/www/
 
