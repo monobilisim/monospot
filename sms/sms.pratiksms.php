0cdcd347fac44f03cc8db6ab6691e6e7844130ef
akin.demirci <akin.demirci@mono.tr>
2025-12-03 11:50:13 +0300

    pratiksms

---
+++ sms/sms.pratiksms.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'api/ekomesaj/SmsApi.php'; // Ekomesaj ile aynı API
+
+function send_sms($user, $password)
+{
+	// Kullanıcı bilgileri
+	$api_username = '';
+	$api_password = '';
+    $api_sender = '';
+
+	global $settings;
+	$message = $settings['name'] . ' WiFi hizmeti icin sifreniz ' . $password . ' olarak tanimlanmistir.';
+
+    $port = 9588;
+    $smsApi = new SmsApi('panel.pratiksms.com.tr', $api_username, $api_password, $port);
+
+    $request = new SendSingleSms();
+    $request->title = 'Monospot';
+    $request->content = $message;
+    $request->number = $user->gsm;
+    $request->encoding = 0;
+    $request->sender = $api_sender;
+
+    $response = $smsApi->sendSingleSms($request);
+
+    if ($response->err == null){
+        return true;
+    } else {
+        return false;
+    }
+}
