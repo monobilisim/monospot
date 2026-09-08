6f8632e5e1fc02f267b2e00eea35d9157af66d7d
Ömer Taha Öztop <taha.oztop@mono.tr>
2026-09-08 13:07:29 +0300

    TT Mesaj için SMS yapılandırması düzenlendi.

--- sms/sms.ttmesaj.php
+++ sms/sms.ttmesaj.php
@@ -2,38 +2,41 @@
 
 function send_sms($user, $password)
 {
-	// Kullanıcı bilgileri
+	// TT Mesaj legacy gateway (FortiAuthenticator ile ayni endpoint)
 	$api_username = '';
 	$api_password = '';
 	$api_origin = '';
+	$api_url = 'http://otpurl.ttmesaj.com/SendSMS/SendSMSURL.aspx';
 
 	global $settings;
-	require_once 'nusoap/lib/nusoap.php';
-	$client = new nusoap_client("http://ws.ttmesaj.com/service1.asmx?wsdl", 'wsdl');
+	// TT Mesaj Turkce karakter kabul etmiyor
 	$message = $settings['name'] . ' WiFi hizmeti icin sifreniz ' . $password . ' olarak tanimlanmistir.';
+
 	$params = array(
-		'username' => $api_username,
-		'password' => $api_password,
-		'numbers' => '90' . $user->gsm,
-		'message' => $message,
-		'origin' => $api_origin,
+		'un' => $api_username,
+		'pw' => $api_password,
+		'msg' => $message,
+		'orgn' => $api_origin,
+		'list' => '90' . $user->gsm,
 	);
 
-	$response = $client->call('sendSingleSMS', $params);
-
-	if (strpos($response['sendSingleSMSResult'], '*OK*') !== false)
-		return true;
-	else
+	$response = ttmesaj_post($api_url, http_build_query($params));
+	if ($response === null)
 		return false;
+
+	// Hata ornegi: WP:-10,Kullanici adinizi...  Basari: *OK* veya WP:0
+	return strpos($response, '*OK*') !== false || strpos($response, 'WP:0') !== false;
 }
 
-function HTTPPoster($url, $data)
+function ttmesaj_post($url, $data)
 {
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
 	$result = curl_exec($ch);
-	return $result;
+	return $result === false ? null : $result;
 }
